3d783a09b6840a89c735b7ada0900a0cad87db7e
David Dales <david.c.dales@gmail.com>
2018-09-04 16:33:55 -0400

    Markdown added

---
+++ readme.md.js
@@ -0,0 +1,45 @@
+ * @fileoverview
+
+A Javascript plugin that presets interactions with primary nav and scrolling
+on a single page website.
+
+ * @author David Dales <david.c.dales@gmail.com>
+ *
+ * @requires jquery
+ *
+ * @example
+   <ul class="scroll-interactor" style="position: fixed">
+
+    <li>
+      <a href="#">
+        <span class="nav-home">Home</span>
+      </a>
+    </li>
+
+    <li>
+      <a href="#">
+        <span class="nav-about">About</span>
+      </a>
+    </li>
+
+    <li>
+      <a href="#">
+        <span class="nav-contact">Contact</span>
+      </a>
+    </li>
+
+  </ul>
+
+  <section id="home"></section>
+  <section id="about"></section>
+  <section id="contact"></section>
+ *
+ * @example
+  var scrollInteractor = new ScrollInteractor({
+    targets: [
+      { $navLink: G('.nav-home'), $target: G('#home') },
+      { $navLink: G('.nav-about'), $target: G('#about') },
+      { $navLink: G('.nav-contact'), $target: G('#contact') },
+    ]
+  });
+  scrollInteractor.init();
